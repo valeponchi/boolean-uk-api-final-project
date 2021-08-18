fc3adef5b1cc207ee2ad330ebfc4669e8b5209b5
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-08-18 10:24:06 +0100

    updated the athlete controller

--- backend/src/resources/athletes/controller.js
+++ backend/src/resources/athletes/controller.js
@@ -1,7 +1,7 @@
 const { athlete } = require('../../utils/dbClient')
 
 async function createOneAthlete(req, res) {
-	const { firstName, lastName, sex, countryId } = req.body
+	const { firstName, lastName, sex, countryId, categoryId } = req.body
 	const newAthlete = {
 		firstName,
 		lastName,
@@ -10,8 +10,16 @@ async function createOneAthlete(req, res) {
 	const createdAthlete = await athlete.create({
 		data: {
 			...newAthlete,
-			category: { connect: { id: parseInt(categoryId) } },
 			country: { connect: { id: parseInt(countryId) } },
+			categories: {
+				create: {
+					categories: {
+						connect: {
+							id: 1,
+						},
+					},
+				},
+			},
 		},
 	})
 	res.json({ data: createdAthlete })
